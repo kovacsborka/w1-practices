648031353ea939299120133433b7125636e39d47
kovacsborka <kovacsborbalaterez@gmail.com>
2022-01-19 19:04:49 +0100

    data handling branch

--- frontend/script.js
+++ frontend/script.js
@@ -1,16 +1,54 @@
+/* const data = [
+    "Gergely Kiss",
+    "Krisztián Bui",
+    "Buczkó Balázs",
+    "András Varga",
+    "Borbála Teréz Kovács"
+];
+ */
+
+const data = [
+    {
+        name: "Gergely Kiss",
+        favoriteFood: "csirke/rizs"
+    },
+    {
+        name: "Krisztián Bui",
+        favoriteFood: "banán"
+    },
+    {
+        name: "Buczkó Balázs",
+        favoriteFood: "kolbászos rájás pizza"
+    },
+    {
+        name: "András Varga",
+        favoriteFood: "brassói"
+    },
+    {
+        name: "Borbála Teréz Kovács",
+        favoriteFood: "avokádó"
+    },
+    {
+        name: "Benett Viszokai",
+        favoriteFood: "alma"
+    },
+    {
+        name: "Krisztián Pörneczi",
+        favoriteFood: "almás pite"
+    },
+]
 function loadEvent() {
     console.log("Az oldal betöltődött");
 
-    const list = document.querySelectorAll("section");
+    const root = document.querySelector("#root")
 
-    for (const elem of list) {
-        elem.insertAdjacentHTML("beforeend", `
-        <p>Text</p> 
-        `);
-       
-        //elem.querySelector("h1").outerHTML = ""
-
-
+    for (const frontendStudent of data) {
+        root.insertAdjacentHTML("beforeend", `
+        <section> 
+            <h1>${frontendStudent.name}</h1>
+            <p>${frontendStudent.favoriteFood}</p>
+        </section>
+        `)
     }
 }
 window.addEventListener("load", loadEvent);
